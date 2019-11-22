90b72eadaabd06e71082deda69a6d029db6e964b
Gopalakrishna <51320774+gopalakrishna306@users.noreply.github.com>
2019-11-22 11:23:47 +0530

    Add files via upload

---
+++ gk_amezon_final_sentimental_analysis.R
@@ -0,0 +1,252 @@
+#install.packages("tm")
+library(tm)
+
+#install.packages("rvest")
+library(rvest)
+#install.packages("magrittr")
+library(magrittr)
+#install.packages("XML")
+library(XML)
+#################################################
+aurl="https://www.amazon.in/OnePlus-McLaren-Limited-Orange-Storage/product-reviews/B07DJCVTBH/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber"
+
+amezon_reviews=NULL
+
+for (i in 1:20){
+  murl=read_html(as.character(paste(aurl,i,sep="=")))
+  rev=murl%>% html_nodes(".review-text")%>%html_text()
+  amezon_reviews=c(amezon_reviews,rev)
+}
+
+amezon_reviews[1:3]
+
+str(amezon_reviews)
+
+
+write.csv(amezon_reviews,file = "amezon_reviews.csv")
+write.csv(amezon_reviews,file = "amezon_reviews.txt")
+
+getwd()
+########################################################################
+
+txt=amezon_reviews
+typeof(txt)
+length(txt)
+
+text=txt
+
+library(tm)
+
+s2 <- iconv(text, "UTF-8", "ASCII", sub = "")
+
+s2
+
+x1=Corpus(VectorSource(s2))
+
+removeURL1 <- function(x) gsub("http[[:alnum:]]*", "", x)
+
+removeURL2 <- function(x) gsub("http[[:alnum:][:punct:]]*", "", x)
+
+removeURL3 <- function(x) gsub("http[^[:space:]]*", "", x)
+
+
+
+x=tm_map(x1,removeURL1)
+x=tm_map(x,removeURL2)
+x=tm_map(x,removeURL3)
+
+inspect(x[180:200])
+x=tm_map(x,tolower)
+x=tm_map(x,removeNumbers)
+x=tm_map(x,removePunctuation)
+x=tm_map(x,removeWords,stopwords("en"))
+x=tm_map(x,stripWhitespace)
+inspect(x[1:5])
+
+
+class(x)
+dataframe <- data.frame(text=sapply(x, identity), 
+                        stringsAsFactors=F)
+
+
+tdm=TermDocumentMatrix(x)
+tdm=as.matrix(tdm)
+
+w=rowSums(tdm)
+head(w)
+tail(w)
+
+w_sub=subset(w,w>15)
+head(w_sub)
+w_sub
+
+sort(w_sub)
+
+nqwrd=c("get","got","see","using","mobile","charge","dont","need","user","review","super","phone","one","oneplus")
+nqwrd
+
+x=tm_map(x,removeWords,nqwrd)
+x=tm_map(x,stripWhitespace)
+inspect(x[1:10])
+
+tdm=TermDocumentMatrix(x)
+tdm=as.matrix(tdm)
+
+w=rowSums(tdm)
+head(w)
+tail(w)
+
+w_sub=subset(w,w>10)
+head(w_sub)
+w_sub
+
+sort(w_sub)
+
+
+##### asume prepossing is completed if want remove some words we can add words to "nqwrd" list
+### word cloud visualization
+
+library(wordcloud)
+library(wordcloud2)
+
+help(wordcloud)
+windows()
+wordcloud(words = names(w_sub),freq = w_sub,colors = rainbow(70)) # morethen 5 times repeted words
+wordcloud(words = names(w),freq = w,colors = rainbow(70)) # total words
+
+
+library(wordcloud2)
+
+#wordcloud2(data, size = 1, minSize = 0, gridSize =  0,
+#           fontFamily = 'Segoe UI', fontWeight = 'bold',
+#           color = 'random-dark', backgroundColor = "white",
+#           minRotation = -pi/4, maxRotation = pi/4, shuffle = TRUE,
+#           rotateRatio = 0.4, shape = 'circle', ellipticity = 0.65,
+#           widgetsize = NULL, figPath = NULL, hoverFunction = NULL)
+#Arguments
+#data	
+#A data frame including word and freq in each column
+
+#size	
+#Font size, default is 1. The larger size means the bigger word.
+
+help("wordcloud2")
+
+
+w2=data.frame(sort(names(w)),sort(w))
+w2
+
+colnames(w2)=c("words","freqence")
+w2
+windows()
+wordcloud2(data=w2,size=2,color = rainbow(70))
+
+w_sub2=data.frame(names(w_sub),w_sub)
+colnames(w_sub2)=c("words","freq")
+wordcloud2(w_sub2,size=1,color = rainbow(70))
+
+#letter cloud
+windows()
+
+letterCloud(w2,word = "s",col=rainbow(50))
+
+
+####### sentimental analysis
+
+data=txt
+
+library(syuzhet)
+
+all_sentences=get_sentences(data)
+
+all_sentences[1:5]
+
+## using bing framme we cam meaure entire 
+bing_sentment=get_sentiment(all_sentences,method = "bing")
+sum(bing_sentment)
+max(bing_sentment)
+min(bing_sentment)
+mean(bing_sentment)
+summary(bing_sentment)
+postive_sentince_number=which.max(bing_sentment)
+Negate_sentience_number=which.min(bing_sentment)
+positive_sentince=all_sentences[postive_sentince_number]
+positive_sentince
+negitive_sentience=all_sentences[Negate_sentience_number]
+negitive_sentience
+
+plot(bing_sentment,main = "plot Trajectory",type = "l",xlab = "bing_semtimens",ylab = "values of bing sentimnet")
+abline(h=0,col="red")
+
+
+### using nrc 
+
+
+emotions=get_nrc_sentiment(all_sentences)
+head(emotions)
+
+
+#anger
+angger_senences=which(emotions$anger>0)
+all_sentences[angger_senences]
+s1=length(angger_senences)
+
+
+# antipation
+antipation_senences=which(emotions$anticipation>0)
+all_sentences[antipation_senences]
+s2=length(antipation_senences)
+
+#
+disgust_senences=which(emotions$disgust>0)
+all_sentences[disgust_senences]
+s3=length(disgust_senences)
+
+
+
+fear_senences=which(emotions$fear>0)
+all_sentences[fear_senences]
+s4=length(fear_senences)
+
+joy_senences=which(emotions$joy>0)
+all_sentences[joy_senences]
+s5=length(joy_senences)
+
+
+sadness_senences=which(emotions$sadness>0)
+all_sentences[sadness_senences]
+s6=length(sadness_senences)
+
+
+surprise_senences=which(emotions$surprise>0)
+all_sentences[surprise_senences]
+s7=length(surprise_senences)
+
+trust_senences=which(emotions$trust>0)
+all_sentences[trust_senences]
+s8=length(trust_senences)
+
+
+
+#### plottionhg and visuvalix=zation
+colSums(emotions)
+df=colSums(data.frame(emotions[1:8]))
+df
+windows()
+barplot(sort(df),col = 1:8,horiz = T,xlab = "persentage of emotion",ylab = "emotions",main = "amezon product-analysis")
+
+print(" amezon product sentimnet analsys is complted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
